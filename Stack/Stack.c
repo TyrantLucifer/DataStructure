c2de7d13d75a18694744f8342984a9df9535a936
tyrantlucifer <tyrantlucifer@gmail.com>
2021-11-03 17:01:28 +0800

    add forma config file

---
+++ Stack/Stack.c
@@ -0,0 +1,6 @@
+/*************************************************************************
+* File Name: Stack.c
+* Author: tyrantlucifer
+* E-mail: tyrantlucifer@gmail.com
+* Blog: https://tyrantlucifer.com
+ ************************************************************************/
